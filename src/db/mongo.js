5e32eaa2b808c724bfbf24fa2ccaa4a66f3c5f2a
bigpaloma <senguenfurkan@gmail.com>
2024-05-08 12:34:48 +0200

    mongo url hardcode

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 import { MONGO_URL } from "$env/static/private"
 
-const client = new MongoClient(MONGO_URL)
+const client = new MongoClient("mongodb+srv://admin:admin@cluster0.okbhkt2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 export function startMongo() {
     console.log("starting Mongo...");
